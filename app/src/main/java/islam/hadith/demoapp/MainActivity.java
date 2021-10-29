1a0b8ff3f2abb99349c0e11b84fbc1060b8833fe
raza_android <razasaeed135@gmail.com>
2021-10-29 20:37:53 +0500

    Button click listeners

--- app/src/main/java/islam/hadith/demoapp/MainActivity.java
+++ app/src/main/java/islam/hadith/demoapp/MainActivity.java
@@ -3,12 +3,31 @@ package islam.hadith.demoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOne; // declaration
+    Button btnTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOne = findViewById(R.id.btnOne); // initialization
+        btnTwo = findViewById(R.id.btnTwo);
+
+        // click listener below
+        btnOne.setOnClickListener(->{
+            Toast.makeText(MainActivity.this, "hello world", Toast.LENGTH_SHORT).show();
+        });
+
+        btnTwo.setOnClickListener(->{
+            Toast.makeText(MainActivity.this, "hello world two", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
